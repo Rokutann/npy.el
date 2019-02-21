c822d986939694a42d21ff9ea01546f54b2c9cf0
mukuge <cyriakus.h@gmail.com>
2019-02-22 02:08:22 +0900

    Add check for sys.path in an inf buf

--- test/npy-integration-test.el
+++ test/npy-integration-test.el
@@ -39,12 +39,21 @@
 ;; For executing tests in Emacs manually.
 ;; (setq npy-test/playground-path "/tmp/npy-playground/")
 
+(setq npy-test/venv-root-for-project1 (s-chomp (shell-command-to-string "(cd /tmp/npy-playground/project1/ && pipenv --venv)")))
+(setq npy-test/venv-root-for-project2 (s-chomp (shell-command-to-string "(cd /tmp/npy-playground/project2/ && pipenv --venv)")))
 
-(ert-deftest npy-integration-test/open-a-file-in-a-project ()
+(ert-deftest npy-integration-test/open-a-file-in-a-project/pipenv-project-root ()
   (with-files-in-playground (("project1/buz.py" . "VAR = 1"))
     (with-file-buffers ("project1/buz.py")
       (set-buffer "buz.py")
       (should (equal npy--pipenv-project-root (@- "project1"))))))
+
+(ert-deftest npy-integration-test/open-a-file-in-a-project/pipenv-virtualenv-root ()
+  (with-files-in-playground (("project1/buz.py" . "VAR = 1"))
+    (with-file-buffers ("project1/buz.py")
+      (set-buffer "buz.py")
+      (npy-update-pipenv-virtualenv-root)
+      (should (equal npy--pipenv-virtualenv-root npy-test/venv-root-for-project1)))))
 
 (ert-deftest npy-integration-test/open-a-file-deep-in-a-project ()
   (with-files-in-playground (("project2/deep/in/the/project/buz.py" . "VAR = 1"))
@@ -67,7 +76,7 @@
       (set-buffer "foo.py")
       (should (equal npy--pipenv-project-root (@- "project1"))))))
 
-(ert-deftest npy-integration-test/open-two-files-in-different-projects ()
+(ert-deftest npy-integration-test/open-two-files-in-different-projects/pipenv-project-root ()
   (with-files-in-playground (("project1/buz.py" . "VAR = 1")
                              ("project2/foo.py" . "VAR = 2"))
     (with-file-buffers ("project1/buz.py" "project2/foo.py")
@@ -76,7 +85,18 @@
       (set-buffer "foo.py")
       (should (equal npy--pipenv-project-root (@- "project2"))))))
 
-(ert-deftest npy-integration-test/spawn-an-inferior-python-buffer ()
+(ert-deftest npy-integration-test/open-two-files-in-different-projects/pipenv-virtualenv-root ()
+  (with-files-in-playground (("project1/buz.py" . "VAR = 1")
+                             ("project2/foo.py" . "VAR = 2"))
+    (with-file-buffers ("project1/buz.py" "project2/foo.py")
+      (set-buffer "buz.py")
+      (npy-update-pipenv-virtualenv-root)
+      (should (equal npy--pipenv-virtualenv-root npy-test/venv-root-for-project1))
+      (set-buffer "foo.py")
+      (npy-update-pipenv-virtualenv-root)
+      (should (equal npy--pipenv-virtualenv-root npy-test/venv-root-for-project2)))))
+
+(ert-deftest npy-integration-test/spawn-an-inferior-python-buffer/check-buffer-name ()
   (with-files-in-playground (("project1/buz.py" . "VAR = 1"))
     (with-file-buffers ("project1/buz.py")
       (set-buffer "buz.py")
@@ -85,6 +105,45 @@
       (npy-helper-wait)
       (let ((python-inf-buf (get-buffer "*Python[v:project1]*")))
         (should-not (eq  python-inf-buf nil))
+        (npy-helper-kill-python-inferior-buffer python-inf-buf)))))
+
+(defvar npy-test/match-flag nil
+  "Non-nil means there has been no successful match.")
+
+(defun npy-helper-match-filter (proc string)
+  "Check if outputs of PROC containg STRING."
+  (when (s-matches-p npy-test/venv-root-for-project1 string)
+    (setq npy-test/match-flag t)))
+
+(defun npy-helper-match-filter (string)
+  "Check if outputs of PROC containg STRING."
+  ;;  (write-region (format "string:%s\n" string) nil "/tmp/npy.log" t)
+  (when (s-matches-p npy-test/venv-root-for-project1 string)
+    ;;    (write-region (format "%s" string) nil "/tmp/npy.log" t)
+    (setq npy-test/match-flag t)))
+
+(ert-deftest npy-integration-test/spawn-an-inferior-python-buffer/check-sys-path ()
+  (with-files-in-playground (("project1/buz.py" . "VAR = 1"))
+    (with-file-buffers ("project1/buz.py")
+      (set-buffer "buz.py")
+      (should (equal npy--pipenv-project-root (@- "project1")))
+      (npy-run-python)
+      (npy-helper-wait)
+      (let ((python-inf-buf (get-buffer "*Python[v:project1]*")))
+        (with-current-buffer python-inf-buf
+          (should-not (eq  python-inf-buf nil))
+          (setq comint-output-filter-functions
+                '(ansi-color-process-output
+                  npy-helper-match-filter
+                  python-shell-comint-watch-for-first-prompt-output-filter
+                  python-pdbtrack-comint-output-filter-function
+                  python-comint-postoutput-scroll-to-bottom
+                  comint-watch-for-password-prompt
+                  ))
+          (setq npy-test/match-flag nil)
+          (python-shell-send-string "import sys\nprint(sys.path)\n")
+          (npy-helper-wait)
+          (should npy-test/match-flag))
         (npy-helper-kill-python-inferior-buffer python-inf-buf)))))
 
 
